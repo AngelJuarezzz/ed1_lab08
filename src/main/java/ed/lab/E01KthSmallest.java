962942ba72eb8956787be435b40f8d8675c44026
Angel <angeljuarez80000@gmail.com>
2026-03-17 12:45:20 -0600

    moscow mule

--- src/main/java/ed/lab/E01KthSmallest.java
+++ src/main/java/ed/lab/E01KthSmallest.java
@@ -2,8 +2,27 @@ package ed.lab;
 
 public class E01KthSmallest {
 
+    private int count;
+    private int result;
+
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+        count = 0;
+        result = 0;
+        inorder(root, k);
+        return result;
     }
 
+    private void inorder(TreeNode<Integer> node, int k) {
+        if (node == null) return;
+
+        inorder(node.left, k);
+
+        count++;
+        if (count == k) {
+            result = node.value;
+            return;
+        }
+
+        inorder(node.right, k);
+    }
 }
